94ebd3965f7fbb38d847c8a42f12660c53d55f4e
Nnamdichukwu <nnamdi.victor.chibueze@gmail.com>
2022-04-17 15:49:56 +0100

    fixed timer

--- contracts/liquiditypool.sol
+++ contracts/liquiditypool.sol
@@ -21,6 +21,7 @@ contract liquidity{
         uint total_withdrawals;
     }
     address public owner;
+    uint private withdrawal_counter;
     mapping(address => uint) public lps;
     mapping (address => PoolInfo) public pool_info;
     mapping (address => Pool_liquidity) public pool_liquidity;
@@ -34,15 +35,15 @@ contract liquidity{
     }
     //event PoolCreation( string indexed owner, string description);
    // event Contribution(string )
-    function createPool(string memory _desc, uint _amount, uint _duration, uint _size, uint _locktime)public requireOwner{
+    function createPool(string memory _desc, uint _amount,uint _duration, uint _size, uint _locktime)public requireOwner{
         require(!pool_info[owner].creator_count[owner]);
         pool_info[msg.sender].creator = msg.sender;
         pool_info[msg.sender].desc = _desc;
         pool_info[msg.sender].amount = _amount * 10 **18 ;
-        pool_info[msg.sender].pool_duration =  block.timestamp+ _duration; 
+        pool_info[msg.sender].pool_duration = block.timestamp + _duration ; 
         pool_info[msg.sender].maxPoolSize = _size;
         pool_info[msg.sender].start_date = block.timestamp ;
-        pool_info[msg.sender].locktime = block.timestamp + _locktime * 1 minutes;
+        pool_info[msg.sender].locktime = block.timestamp + _locktime;
     }
     function getTime() public view returns(uint){
        return pool_info[msg.sender].start_date ;
@@ -50,7 +51,7 @@ contract liquidity{
     function joinPool(address _to) public payable {
         require (_to == owner);
         require (msg.value * 10**18 >= pool_info[owner].amount, "too small");
-        require(    block.timestamp <   pool_info[msg.sender].pool_duration, "time has elapsed");
+        require( block.timestamp <  pool_info[owner].pool_duration , "time has elapsed");
         require (msg.value * 10**18 >= pool_info[owner].amount, "too small");
         require(pool_liquidity[owner].totalContributors < pool_info[owner].maxPoolSize);
         uint balance = msg.value- pool_info[_to].amount   ;
@@ -77,7 +78,7 @@ contract liquidity{
          require (_to == owner);
         require(lps[msg.sender] >= 1);
        require (msg.value * 10**18 >= pool_info[owner].amount, "too small");
-       require(pool_info[msg.sender].start_date < pool_info[msg.sender].pool_duration );
+       require(block.timestamp > withdrawal_counter);
         uint balance = msg.value- pool_info[_to].amount   ;
         lps[msg.sender] = 1;
         uint amount = msg.value - balance; 
@@ -99,11 +100,12 @@ contract liquidity{
       } 
     }
    function withdrawal(address _to, address from) public payable {
-       require( block.timestamp > pool_info[msg.sender].start_date +  pool_info[msg.sender].locktime);
+       require( block.timestamp >  pool_info[owner].locktime);
        require(!pool_liquidity[from].paid_out[msg.sender]);
        require( pool_liquidity[_to].contributors[msg.sender]> 0);
        uint amount = pool_liquidity[from].totalLiquidity;
        pool_liquidity[from].paid_out[msg.sender] = true;
        payable(address(_to)).transfer(amount); 
+       withdrawal_counter = block.timestamp;
    }
 }
